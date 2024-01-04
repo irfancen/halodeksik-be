6fb44e388bdcb47d95aef27067e4cdddc435dce1
Muhammad Irfan Junaidi <mirfanjunaidi@gmail.com>
2024-01-04 22:36:13 +0700

    feat: add parenthesis for where clause

--- app/appdb/where_clause.go
+++ app/appdb/where_clause.go
@@ -1,10 +1,12 @@
 package appdb
 
 type WhereClause struct {
-	Column    string
-	Condition DBCondition
-	Value     interface{}
-	Logic     DBLogic
+	Column           string
+	Condition        DBCondition
+	Value            interface{}
+	Logic            DBLogic
+	OpenParenthesis  bool
+	CloseParenthesis bool
 }
 
 func NewWhere(column string, condition DBCondition, value interface{}, logic ...DBLogic) WhereClause {
@@ -12,4 +14,11 @@ func NewWhere(column string, condition DBCondition, value interface{}, logic ...
 		return WhereClause{Column: column, Condition: condition, Value: value, Logic: logic[0]}
 	}
 	return WhereClause{Column: column, Condition: condition, Value: value}
+}
+
+func NewWhereParenthesis(column string, condition DBCondition, value interface{}, open bool, close bool, logic ...DBLogic) WhereClause {
+	if len(logic) > 0 {
+		return WhereClause{Column: column, Condition: condition, Value: value, OpenParenthesis: open, CloseParenthesis: close, Logic: logic[0]}
+	}
+	return WhereClause{Column: column, Condition: condition, Value: value, OpenParenthesis: open, CloseParenthesis: close}
 }
